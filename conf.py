285be63978e74777332544ace409d88349707ed7
EnvisionIoT <37394998+EnvisionIot@users.noreply.github.com>
2019-04-18 14:01:27 +0800

    Add files via upload

--- conf.py
+++ conf.py
@@ -20,7 +20,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +33,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -39,7 +42,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'm2r']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,10 +52,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 
-source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
-}
-
 # source_suffix = ['.rst', '.md']
 source_suffix = ['.rst', '.md']
 
@@ -59,9 +59,9 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = 'EnOS Tutorials'
-copyright = '2018, EnOS'
-author = 'Envision Digital'
+project = u'EnOS Tutorials'
+copyright = u'2018, EnOS'
+author = u'Envision Digital'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -96,7 +96,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_enos_theme'
 
 #import alabaster
 
@@ -141,35 +141,137 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'EnOSDocumentationCenterdoc'
+htmlhelp_basename = u'EnOSDocumentationCenterdoc'
 
+
+latex_logo = "bg.png"
 
 # -- Options for LaTeX output ---------------------------------------------
-
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
+    # Sonny, Lenny, Glenn, Conny, Rejne, Bjarne and Bjornstrup
+    # 'fncychap': '\\usepackage[Lenny]{fncychap}',
+    'fncychap': '\\usepackage{fncychap}',
+    'fontpkg': '\\usepackage{amsmath,amsfonts,amssymb,amsthm}',
 
+    'figure_align':'htbp',
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    # 'preamble': '',
+    'preamble': r'''
+        \setcounter{secnumdepth}{3}
+        \setcounter{tocdepth}{2}
+        \usepackage{amsmath,amsfonts,amssymb,amsthm}
+        \usepackage{graphicx}
+        \usepackage[notlot,nottoc,notlof]{}
+        \usepackage{color}
+        \usepackage{transparent}
+        \usepackage{eso-pic}
+        \usepackage{lipsum}
+        \usepackage{footnotebackref}
+        \usepackage{mathptmx}
+        \usepackage{anyfontsize}
+        \usepackage{t1enc}
+        \usepackage{setspace}
+        \singlespacing
+        \usepackage{datetime}
+        \usepackage{eso-pic,graphicx}
+        \newdateformat{MonthYearFormat}{%
+            \monthname[\THEMONTH], \THEYEAR}
+        \usepackage{fancyhdr}
+        \pagestyle{fancy}
+        \fancyhf{}
 
+        \fancyhead[L]{\ifthenelse{\isodd{\value{page}}}{ \small \nouppercase{\leftmark} }{}}
+        \fancyhead[R]{\ifthenelse{\isodd{\value{page}}}{}{ \small \nouppercase{\rightmark} }}
+
+        %\fancyhead[RO]{\small \nouppercase{\rightmark}}
+        %\fancyhead[LE]{\small \nouppercase{\leftmark}}
+
+        \fancyfoot[R]{\ifthenelse{\isodd{\value{page}}}{{\tiny EnOS Tutorials} }{\href{https://www.envisioniot.com}{\tiny Envision digital}}}
+
+        %%% page number
+        \fancyfoot[CO, CE]{\thepage}
+
+        \renewcommand{\headrulewidth}{0.5pt}
+        \renewcommand{\footrulewidth}{0.5pt}
+
+        \RequirePackage{tocbibind} %%% comment this to remove page number for following
+        \addto\captionsenglish{\renewcommand{\contentsname}{Table of contents}}
+        \addto\captionsenglish{\renewcommand{\listfigurename}{List of figures}}
+        \addto\captionsenglish{\renewcommand{\listtablename}{List of tables}}
+        % \addto\captionsenglish{\renewcommand{\chaptername}{Chapter}}
+
+
+        %%reduce spacing for itemize
+        \usepackage{enumitem}
+        \setlist{nosep}
+
+        %%%%%%%%%%% Quote Styles at the top of chapter
+        \usepackage{epigraph}
+        \setlength{\epigraphwidth}{0.8\columnwidth}
+        \newcommand{\chapterquote}[2]{\epigraphhead[60]{\epigraph{\textit{#1}}{\textbf {\textit{--#2}}}}}
+        %%%%%%%%%%% Quote for all places except Chapter
+        \newcommand{\sectionquote}[2]{{\quote{\textit{``#1''}}{\textbf {\textit{--#2}}}}}
+    ''',
+
+
+    'maketitle': r'''
+        \pagenumbering{Roman} %%% to avoid page 1 conflict with actual page 1
+
+        \begin{titlepage}
+            \AddToShipoutPictureBG*{\includegraphics[width=\paperwidth,height=\paperheight]{bg.png}}
+
+            \vspace*{15mm} %%% * is used to give space from top
+            \fontsize{40pt}{\baselineskip}\selectfont \textbf{EnOS}
+
+            \vspace{5mm}
+
+            \fontsize{40pt}{\baselineskip}\selectfont \textbf{Tutorials}
+
+            \vspace{5mm}
+
+            \huge \textmd{Version}\textbf{ 1.0}
+            %% \vfill adds at the bottom
+            \vfill
+            \centerline{\fontsize{16pt}{\baselineskip}\selectfont \textbf{Envision Digital}}
+            \vspace{3mm}
+            \centerline{\fontsize{16pt}{\baselineskip}\selectfont \textmd{\today}}
+        \end{titlepage}
+
+        \clearpage
+        \pagenumbering{roman}
+        # \tableofcontents
+        # %%%\listoffigures
+        # %% \listoftables
+        \clearpage
+        \pagenumbering{arabic}
+
+        ''',
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+    'sphinxsetup': \
+        'hmargin={0.7in,0.7in}, vmargin={1in,1in}, \
+        verbatimwithframe=true, \
+        TitleColor={rgb}{0,0,0}, \
+        HeaderFamily=\\rmfamily\\bfseries, \
+        InnerLinkColor={rgb}{0,0,1}, \
+        OuterLinkColor={rgb}{0,0,1}',
+        'tableofcontents':' ',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'EnOSDocumentationCenter.tex', 'EnOS Tutorials',
-     'Envision Digital', 'manual'),
+    (master_doc, u'EnOSDocumentationCenter.tex', u'EnOS Tutorials',
+     u'Envision Digital', 'book', 'true'),
 ]
 
 
@@ -178,7 +280,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'enosdocumentationcenter', 'EnOS Tutorials',
+    (master_doc, u'enosdocumentationcenter', u'EnOS Tutorials',
      [author], 1)
 ]
 
@@ -220,7 +322,3 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-# custom css
-def setup(app):
-    app.add_stylesheet('css/custom.css')
